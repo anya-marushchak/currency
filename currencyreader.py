4814012bd9e6a030327354b8bf3a8ebf10d4ca71
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-13 20:16:21 +0100

    Edit file

--- currencyreader.py
+++ currencyreader.py
@@ -10,27 +10,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-import os
-
-os.path.join(os.getcwd(), 'rates.csv')
-
-
 @app.route("/currency", methods=["GET", "POST"])
 def currency():
   if request.method == "POST":
     data = request.form
     code = data.get('code')
-    amount = data.get("amount")
-    
+    amount = float(data.get("amount"))
+   
 
-    if code == code["USD"]:
-      return amount*4.2534
-    if code == code["AUD"]:
-      return amount*3.1916
-    if code == code["CAD"]:
-      return amount*3.1916
-    if code == code["EUR"]:
-      return amount*4.6388
+    if code in rates.json:
+      return render_template("index.html", amount = amount*rates.json[code])
+    
  
   return render_template('index.html', amount=0)
 
